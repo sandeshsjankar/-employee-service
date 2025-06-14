5a50d24eb5c341a525837eba05afbe687597014b
sandeshsjankar <sandeshsjankar@gmail.com>
2025-06-13 20:46:58 -0700

    first commit

---
+++ src/main/java/com/employee/entity/Employee.java
@@ -0,0 +1,75 @@
+package com.employee.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+public class Employee {
+	public Employee(Long id, String name, String email, Integer salary) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+	}
+
+	public Employee() {
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", email='" + email + '\'' +
+				", salary=" + salary +
+				'}';
+	}
+
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String email;
+	private Integer salary;
+
+}
